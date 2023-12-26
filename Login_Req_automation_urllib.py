7e734f2451f321821d37b4ad74fecf31c7cea7c8
KeerthanHM00 <111059262+KeerthanHM00@users.noreply.github.com>
2023-12-26 21:52:20 +0530

    Update Login_Req_automation_urllib.py

--- Login_Req_automation_urllib.py
+++ Login_Req_automation_urllib.py
@@ -4,26 +4,27 @@ import gzip
 
 # Define the target URL
 url = "http://139.144.1.145/index.php/index/login/signIn"
-
 # Print the headers for the POST request
 headers = {
-    "Host": "testphp.vulnweb.com",
-    "Content-Length": "28",
+    "Host": "139.144.1.145",
+    "Content-Length": "93",
     "Cache-Control": "max-age=0",
     "Upgrade-Insecure-Requests": "1",
-    "Origin": "http://testphp.vulnweb.com",
-    "Content-Type": "application/x-www-form-urlencoded",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.71 Safari/537.36",
+    "Origin": "http://139.144.1.145",
+    "Content-Type": "application/x-www-form-urlencoded",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Referer": "http://testphp.vulnweb.com/search.php?test=query",
+    "Referer": "http://139.144.1.145/index.php/index/login",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "en-US,en;q=0.9",
+    "Cookie": "OJSSID=06nnugivpmcpq386r47bafr9bp",
     "Connection": "close",
 }
 
 # POST data
-search= str(input("what do you want to search for: "))
-post_data = "searchFor="+search+"&goButton=go"
+usr_name= str(input("username: ")) #username accepting variable
+pswd= str(input("password: ")) #password accepting variable
+post_data = "csrfToken=d570b82aff7b044367832cf895d6589d&source=&username="+usr_name+"&password="+pswd+"&remember=1"
 print(type(post_data))
 encoded_data = post_data.encode('utf-8')
 
@@ -33,7 +34,7 @@ for key, value in headers.items():
 
 # Send a POST request
 try:
-    request = urllib.request.Request(f"{url}/search.php?test=query", data=encoded_data, headers=headers, method='POST')
+    request = urllib.request.Request(url, data=encoded_data, headers=headers, method='POST')
     response = urllib.request.urlopen(request)
 
 
